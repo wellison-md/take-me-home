9e38038a9354948ab498fe8d834bf9460015fdc3
wellison m. dias <wellisonmdias@gmail.com>
2023-12-28 17:07:08 -0300

    :recycle: refact: fix code style

--- frontend/src/components/common/flex/flexStyle.js
+++ frontend/src/components/common/flex/flexStyle.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   align-items: center;
   display: flex;
-  flex-direction: ${ (props) => props.orientation ?
-    props.orientation : 'row' };
+  flex-direction: ${ (props) => props.orientation ? props.orientation : 'row' };
   flex-wrap: wrap;
   justify-content: center;
 `;
