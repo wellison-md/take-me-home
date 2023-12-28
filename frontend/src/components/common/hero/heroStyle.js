f5420296947efce6c4922d63a4f4dfb13cc88f8b
wellison m. dias <wellisonmdias@gmail.com>
2023-12-27 23:46:59 -0300

    :zap: feat: add hero-component

---
+++ frontend/src/components/common/hero/heroStyle.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeroContainer = styled.div`
+  & img {
+    margin-top: 135px;
+    margin-left: 250px;
+    margin-right: 250px;
+    height: 397px;
+    border-radius: 8px;
+  }
+`;
